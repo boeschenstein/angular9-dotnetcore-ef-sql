a5fae786db9a12a6f594527de503847cf55d6210
bop <patrik.boeschenstein@trivadis.com>
2020-04-24 13:45:07 +0200

    source code added

--- myAngular9NetCore3AppEFSql/backend/Controllers/WeatherForecastController.cs
+++ myAngular9NetCore3AppEFSql/backend/Controllers/WeatherForecastController.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyBackend.EF;
 
 namespace MyBackend.Controllers
 {
@@ -26,6 +26,38 @@ namespace MyBackend.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            // TODO: REMOVE THIS CODE !! this is for test purposes only!!! -- START
+            using (var db = new BloggingContext())
+            {
+                // Create
+                Console.WriteLine("Inserting a new blog");
+                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                db.SaveChanges();
+
+                // Read
+                Console.WriteLine("Querying for a blog");
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+
+                // Update
+                Console.WriteLine("Updating the blog and adding a post");
+                blog.Url = "https://devblogs.microsoft.com/dotnet";
+                blog.Posts.Add(
+                    new Post
+                    {
+                        Title = "Hello World",
+                        Content = "I wrote an app using EF Core!"
+                    });
+                db.SaveChanges();
+
+                // Delete
+                Console.WriteLine("Delete the blog");
+                db.Remove(blog);
+                db.SaveChanges();
+            }
+            // TODO: REMOVE THIS CODE !! this is for test purposes only!!! -- END
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
